8c2beeac0e595b65d1abe14ad4780005c17d12c3
almidi <vailakis7@yahoo.gr>
2018-03-15 01:52:13 +0200

    Lock needed for parallel char computation

--- Project1/Proj1OMP/main.c
+++ Project1/Proj1OMP/main.c
@@ -3,12 +3,16 @@
 #include <time.h>
 #include <omp.h>
 
+int hamming(int l , int* a, int* b);
+int timedif(struct timespec *start, struct timespec *stop);
+
+
 int main(int argc, char **argv) {
-    int m, n,l, t, i, j;             // m = A strings, n = B strings , l = string length
-    int **a, **b, **distSerial , **distR , **distC, **distRC;    // Tables
+    int m, n,l, t, i, j, k;             // m = A strings, n = B strings , l = string length
+    int **a, **b, **distSerial , **distR , **distC, **distH;    // Tables
     srand(time(NULL));          // init rand
-    clock_t start,timeA, timeB, timeC, timeD ;
-    float serialT , rowT, columnT, cellT ;
+    struct timespec start,timeA, timeB, timeC, timeD ;
+    int  serialT , rowT, columnT, cellT ;
 
 
 //  Check arguments
@@ -23,27 +27,29 @@ int main(int argc, char **argv) {
     l = atoi(argv[3]);
     t = atoi(argv[4]);
 
-    printf("Argument: m = %d\n",m);
-    printf("Argument: n = %d\n",n);
-    printf("Argument: l = %d\n",l);
-    printf("Argument: t = %d\n",t);
-    printf("-----------------\n\n");
+    printf("\nArguments");
+    printf("\n-------------------");
+    printf("\n     m: %d",m);
+    printf("\n     n: %d",n);
+    printf("\n     l: %d",l);
+    printf("\n     t: %d\n",t);
 
 //  OpenMP Thread Limit
     omp_set_num_threads(t);
+
 
 
 //  Init Tables
     distSerial = malloc(m*sizeof(int*));
     distC = malloc(m*sizeof(int*));
     distR = malloc(m*sizeof(int*));
-    distRC = malloc(m*sizeof(int*));
+    distH = malloc(m*sizeof(int*));
 
     for (i=0;i<m;i++){
         distSerial[i]=malloc(n*sizeof(int));
         distR[i]=malloc(n*sizeof(int));
         distC[i]=malloc(n*sizeof(int));
-        distRC[i]=malloc(n*sizeof(int));
+        distH[i]=malloc(n*sizeof(int));
     }
 
     a = malloc(m*sizeof(int*));
@@ -55,7 +61,6 @@ int main(int argc, char **argv) {
     for (i=0;i<n;i++){
         b[i]=malloc(l*sizeof(int));
     }
-
 
 
 //  init A set
@@ -75,48 +80,57 @@ int main(int argc, char **argv) {
 
 //  HAMMING
 
-    start = clock();
+    clock_gettime(CLOCK_REALTIME,&start);
 
-//    No Parallelization
+//  No Parallelization
     for (i=0 ; i<m; i++){
         for (j=0;j<n;j++){
             distSerial[i][j] = hamming(l,a[i],b[j]);
         }
     }
 
-    timeA = clock();
+    clock_gettime(CLOCK_REALTIME,&timeA);
 
-//    Parallelize each row
-    #pragma omp parallel for
-    for (int i=0 ; i<m; i++){
-        for (int j=0;j<n;j++){
+//  Parallelize each row
+//  Each task take a string from array "a" and
+//  processes it with every string from array "b".
+    #pragma omp parallel for schedule(dynamic) private(j)
+    for (i=0 ; i<m; i++){
+        for (j=0;j<n;j++){
             distR[i][j] = hamming(l,a[i],b[j]);
         }
     }
 
-//    #pragma omp barrier
-    timeB = clock();
+    clock_gettime(CLOCK_REALTIME,&timeB);
 
-//    Parallelize each column
-    #pragma omp parallel for collapse(1)
-    for (int j=0 ; j<n; j++){
-        for (int i=0;i<m;i++){
+//  Parallelize each cell
+//  Each task takes a  string from array "a" and
+//  processes it with a string from array "b".
+    #pragma omp parallel for collapse(2) private(i,j)
+    for (i=0 ; i<m; i++){
+        for (j=0;j<n;j++){
             distC[i][j] = hamming(l,a[i],b[j]);
         }
     }
-//    #pragma omp barrier
-    timeC = clock();
 
-//    Parallelize each cell
-    #pragma omp parallel for collapse(2)
-    for (int i=0 ; i<m; i++){
-        for (int j=0;j<n;j++){
-            distRC[i][j] = hamming(l,a[i],b[j]);
+    clock_gettime(CLOCK_REALTIME,&timeC);
+
+//  Parallelize each string
+//  Each task takes a character from a string from array "a" and processes it
+//  with the corresponding character from a string in array "b".
+    #pragma omp parallel for collapse(3) private(i,j,k)
+    for (i=0 ; i<m; i++){
+        for (j=0;j<n;j++){
+            for (k=0;k<l;k++){
+                if (a[i][k] != b[j][k]){
+
+                    distH[i][j]++;
+                }
+            }
         }
     }
-//    #pragma omp barrier
-    timeD = clock();
 
+    clock_gettime(CLOCK_REALTIME,&timeD);
 
 //  Check Hamming Distances
     for (i=0 ; i<m; i++){
@@ -126,42 +140,50 @@ int main(int argc, char **argv) {
                 return (-1);
             }
             if (distSerial[i][j] != distC[i][j]){
-                printf("Column Parallelization Data Mismatch");
+                printf("Cell Parallelization Data Mismatch");
                 return (-1);
             }
-            if (distSerial[i][j] != distRC[i][j]){
-                printf("Cell Parallelization Data Mismatch");
+            if (distSerial[i][j] != distH[i][j]){
+                printf("String Parallelization Data Mismatch");
                 return (-1);
             }
         }
     }
 
+//  Calculate excecution times
+    serialT =  timedif(&start,&timeA);
+    rowT = timedif(&timeA,&timeB);
+    columnT = timedif(&timeB,&timeC);
+    cellT = timedif(&timeC,&timeD);
 
-
-    serialT = (float)((timeA - start) * 1000)/ CLOCKS_PER_SEC;
-    rowT = (float)((timeB - timeA) * 1000)/ CLOCKS_PER_SEC;
-    columnT = (float)((timeC - timeB) * 1000)/ CLOCKS_PER_SEC;
-    cellT = (float)((timeD - timeC) * 1000)/ CLOCKS_PER_SEC;
-
-
-    printf("\nSerial    Time :%f ms.",serialT);
-    printf("\nRow       Time :%f ms.",rowT);
-    printf("\nColumn    Time :%f ms.",columnT);
-    printf("\nCell      Time :%f ms.",cellT);
+//  Print excecution times
+    printf("\nComputation Times");
+    printf("\n-------------------");
+    printf("\nSerial: %d ms",serialT);
+    printf("\n   Row: %d ms",rowT);
+    printf("\nColumn: %d ms",columnT);
+    printf("\n  Char: %d ms\n",cellT);
 
     return 0;
 
-}
 
+}
 
 //Hamming Distance Calculator between two arrays of l size
 int hamming(int l , int* a, int* b){
     int k = 0;
     for (int i=0;i<l;i++){
-        if (a[i] == b[i]){
+        if (a[i] != b[i]){
             k++;
         }
     }
     return k;
 }
+
+
+int timedif(struct timespec *start, struct timespec *stop){
+    return((int)((stop->tv_sec - start->tv_sec)*1000)+(float)((stop->tv_nsec - start->tv_nsec)/1000000));
+}
+
+
 
