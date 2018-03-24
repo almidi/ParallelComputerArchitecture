0d0e455b4d4dd555994ce1a82572e8905e018762
almidi <a.vailakis@gmail.com>
2018-03-24 05:04:13 +0200

    Pthreads SegFault. Thead spawning must be limited

--- Project1/Proj1PT/cmake-build-debug/CMakeFiles/Proj1PT.dir/DependInfo.cmake
+++ Project1/Proj1PT/cmake-build-debug/CMakeFiles/Proj1PT.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/hdd/TUC/ParallelCompArch/ParallelComputerArchitecture/Project1/Proj1PT/main.c" "/hdd/TUC/ParallelCompArch/ParallelComputerArchitecture/Project1/Proj1PT/cmake-build-debug/CMakeFiles/Proj1PT.dir/main.c.o"
+  "/home/almidi/TUC/ParallelComputerArchitecture/Project1/Proj1PT/main.c" "/home/almidi/TUC/ParallelComputerArchitecture/Project1/Proj1PT/cmake-build-debug/CMakeFiles/Proj1PT.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- Project1/Proj1PT/main.c
+++ Project1/Proj1PT/main.c
@@ -1,32 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 #include <time.h>
 
+
+void *rowThread(void *threadarg);
+void *cellThread(void *threadarg);
+void *charThread(void *threadarg);
+int hamming(int l , int* a, int* b);
+
+struct rowThreadData{
+    int  tid;
+    int  n;
+    int  row;
+    int  l;
+    int** a;
+    int** b;
+    int** distR;
+};
+
+struct cellThreadData{
+    int  tid;
+    int  row;
+    int  cell;
+    int  l;
+    int** a;
+    int** b;
+    int** distC;
+};
+
+struct charThreadData{
+    int  tid;
+    int  row;
+    int  cell;
+    int  l;
+    int** a;
+    int** b;
+    int** distCH;
+};
+
 int main(int argc, char **argv) {
-    int m,n,l,i,j ;             // m = A strings, n = B strings , l = string length
-    int **a , **b , **dist ;    // Tables
+    int m,n,l, t ,i,j ;             // m = A strings, n = B strings , l = string length
+    int **a, **b, **distSerial , **distR , **distC, **distCH ;    // Tables
     srand(time(NULL));          // init rand
 
+    //Thread data
+    struct rowThreadData *rowTD;
+    struct cellThreadData *cellTD;
+    struct charThreadData *charTD;
+
+    //Threads
+    pthread_t *rowT;
+    pthread_t *cellT;
+    pthread_t *charT;
+
+
 //  Check arguments
-    if (argc != 4){
+    if (argc != 5){
         printf("Invalid Arguments");
         return -1;
     }
 
 //  Assign Arguments
     m = atoi(argv[1]);
-    n = atoi (argv[2]);
-    l = atoi (argv[3]);
+    n = atoi(argv[2]);
+    l = atoi(argv[3]);
+    t = atoi(argv[4]);
 
-    printf("Argument: m = %d\n",m);
-    printf("Argument: n = %d\n",n);
-    printf("Argument: l = %d\n",l);
-    printf("-----------------\n\n");
+    printf("\nArguments");
+    printf("\n-------------------");
+    printf("\n     m: %d",m);
+    printf("\n     n: %d",n);
+    printf("\n     l: %d",l);
+    printf("\n     t: %d\n",t);
 
 //  Init Tables
-    dist = malloc(m*sizeof(int*));
+    distSerial = malloc(m*sizeof(int*));
+    distC = malloc(m*sizeof(int*));
+    distR = malloc(m*sizeof(int*));
+    distCH = malloc(m*sizeof(int*));
+    rowTD = malloc(m*sizeof(struct rowThreadData*));
+    cellTD = malloc(m*n*sizeof(struct cellThreadData*));
+    charTD = malloc(m*n*l*sizeof(struct charThreadData*));
+    rowT = malloc(m*sizeof(struct pthread_t*));
+    cellT = malloc(m*n*sizeof(struct pthread_t*));
+    charT = malloc(m*n*l*sizeof(struct pthread_t*));
+
     for (i=0;i<m;i++){
-        dist[i]=malloc(n*sizeof(int));
+        distSerial[i]=malloc(n*sizeof(int));
+        distR[i]=malloc(n*sizeof(int));
+        distC[i]=malloc(n*sizeof(int));
+        distCH[i]=malloc(n*sizeof(int));
+    }
+
+    for (i=0 ; i<m; i++){
+        for (j=0;j<n;j++){
+            distR[i][j] = 0;
+            distC[i][j] = 0;
+            distCH[i][j] = 0;
+        }
     }
 
     a = malloc(m*sizeof(int*));
@@ -53,14 +125,69 @@ int main(int argc, char **argv) {
             b[i][j] = rand() % 2 ; // random number between 0 and 1
         }
     }
+
+
+//  Calculate Serial Hamming
     for (i=0 ; i<m; i++){
         for (j=0;j<n;j++){
-            dist[i][j] = hamming(l,a[i],b[j]);
-            //printf("%d ",dist[i][j]);
+            distSerial[i][j] = hamming(l,a[i],b[j]);
         }
-        //printf("\n");
     }
+
+//  Calculate Serial Hamming, Parallelize each row
+    for (i=0 ; i<m; i++){
+        rowTD[i].tid = i ;
+        rowTD[i].row = i ;
+        rowTD[i].a = a ;
+        rowTD[i].b = b ;
+        rowTD[i].distR = distR;
+        rowTD[i].l = l;
+        rowTD[i].n = n;
+        pthread_create(&rowT[i], NULL, rowThread, (void *) &rowTD[i]);
+
+    }
+
     return 0;
+}
+
+
+
+void *rowThread(void *threadarg){
+    int **dist;
+    int i ,j ,k, l, n ,row, h=0;
+    struct rowThreadData *args;
+    int **a, **b;
+
+    args = (struct rowThreadData *) threadarg;
+    dist = args->distR;
+    a = args->a;
+    b = args->b;
+    l = args->l;
+    n = args->n;
+    row = args->row;
+
+
+
+    for(i=0;i<n;i++){
+        //dist[args->row][i] = hamming(10,a[args->row],b[i]);
+        k = 0;
+        for ( j=0;j<50;j++){
+//            if (a[row][j] == b[i][j]){
+//                k=1;
+//            }
+        }
+        dist[row][i] = k;
+    }
+
+    pthread_exit(NULL);  
+}
+
+void *cellThread(void *threadarg){
+
+}
+
+void *charThread(void *threadarg){
+
 }
 
 //Hamming Distance Calculator between two arrays of l size
