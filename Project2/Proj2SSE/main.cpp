c806200326e803b4eae948ce529b7d6f5152fe4b
almidi <a.vailakis@gmail.com>
2018-04-25 01:14:11 +0300

    Project 2 SSE vectors

--- Project2/Proj2SSE/main.cpp
+++ Project2/Proj2SSE/main.cpp
@@ -33,23 +33,24 @@ int main(int argc, char **argv) {
 
     // works serialized with mm_malloc
     float *mVec = (float*)_mm_malloc(sizeof(float)*N, 16);
-    __m128i * mVec_ptr = (__m128i*) mVec;
+    __m128 * mVec_ptr = (__m128*) mVec;
     assert(mVec != NULL);
     float *nVec = (float*)_mm_malloc(sizeof(float)*N, 16);
-    __m128i * nVec_ptr = (__m128i*) nVec;
+    __m128 * nVec_ptr = (__m128*) nVec;
     assert(nVec != NULL);
     float *LVec = (float*)_mm_malloc(sizeof(float)*N, 16);
-    __m128i * LVec_ptr = (__m128i*) LVec;
+    __m128 * LVec_ptr = (__m128*) LVec;
     assert(LVec != NULL);
     float *RVec = (float*)_mm_malloc(sizeof(float)*N, 16);
-    __m128i * RVec_ptr = (__m128i*) RVec;
+    __m128 * RVec_ptr = (__m128*) RVec;
     assert(RVec != NULL);
     float *CVec = (float*)_mm_malloc(sizeof(float)*N, 16);
-    __m128i * CVec_ptr = (__m128i*) CVec;
+    __m128 * CVec_ptr = (__m128*) CVec;
     assert(CVec != NULL);
     float *FVec = (float*)_mm_malloc(sizeof(float)*N, 16);
-    __m128i * FVec_ptr = (__m128i*) FVec;
+    __m128 * FVec_ptr = (__m128*) FVec;
     assert(FVec != NULL);
+
 
 
     for (int i = 0; i < N; i++) {
@@ -76,22 +77,85 @@ int main(int argc, char **argv) {
     }
     float maxF = 0.0f;
     double timeTotal = 0.0f;
+
+    __m128 * vec_num = (__m128*)_mm_malloc(sizeof(float)*4, 16);
+    __m128 * vec_num_0 = (__m128*)_mm_malloc(sizeof(float)*4, 16);
+    __m128 * vec_num_1 = (__m128*)_mm_malloc(sizeof(float)*4, 16);
+    __m128 * vec_num_2 = (__m128*)_mm_malloc(sizeof(float)*4, 16);
+    __m128 * vec_den = (__m128*)_mm_malloc(sizeof(float)*4, 16);
+    __m128 * vec_den_0 = (__m128*)_mm_malloc(sizeof(float)*4, 16);
+    __m128 * vec_den_1 = (__m128*)_mm_malloc(sizeof(float)*4, 16);
+
+    float *onesVec = (float*)_mm_malloc(sizeof(float)*N, 16);
+    __m128 * onesVec_ptr = (__m128*) CVec;
+    assert(onesVec != NULL);
+    float *twosVec = (float*)_mm_malloc(sizeof(float)*N, 16);
+    __m128 * twosVec_ptr = (__m128*) FVec;
+    assert(twosVec != NULL);
+    float *pponesVec = (float*)_mm_malloc(sizeof(float)*N, 16);
+    __m128 * pponesVec_ptr = (__m128*) FVec;
+    assert(pponesVec != NULL);
+
+    for (int i = 0 ; i < 4 ; i++){
+        onesVec[i] = 1.0;
+        pponesVec[i] = 0.01;
+        twosVec[i] = 2.0;
+    }
+
     for (int j = 0; j < iters; j++) {
         double time0 = gettime();
 
 
         //To be SIMDed
+        for (int i = 0; i < N/4; i++) {
+
+//            float num_0 = LVec[i] + RVec[i];
+//            float num_1 = mVec[i] * (mVec[i] - 1.0) / 2.0;
+//            float num_2 = nVec[i] * (nVec[i] - 1.0) / 2.0;
+//            float num = num_0 / (num_1 + num_2);
+//            float den_0 = CVec[i] - LVec[i] - RVec[i];
+//            float den_1 = mVec[i] * nVec[i];
+//            float den = den_0 / den_1;
+//            FVec[i] = num / (den + 0.01);
+
+            //float num_0 = LVec[i] + RVec[i];
+            *vec_num_0 = _mm_add_ps(LVec_ptr[i], RVec_ptr[i]);
+
+            //float num_1 = mVec[i] * (mVec[i] - 1.0) / 2.0;
+            *vec_num_1 = _mm_sub_ps(mVec_ptr[i], *onesVec_ptr);
+            *vec_num_1 = _mm_div_ps(*vec_num_1,*twosVec_ptr);
+            *vec_num_1 = _mm_mul_ps(*vec_num_1,mVec_ptr[i]);
+
+            //float num_2 = nVec[i] * (nVec[i] - 1.0) / 2.0;
+            *vec_num_2 = _mm_sub_ps(nVec_ptr[i], *onesVec_ptr);
+            *vec_num_2 = _mm_div_ps(*vec_num_2,*twosVec_ptr);
+            *vec_num_2 = _mm_mul_ps(*vec_num_2,nVec_ptr[i]);
+
+
+            //float num = num_0 / (num_1 + num_2);
+            *vec_num = _mm_add_ps(*vec_num_1, *vec_num_2);
+            *vec_num = _mm_div_ps(*vec_num_0,*vec_num);
+
+            //float den_0 = CVec[i] - LVec[i] - RVec[i];
+            *vec_den_0 = _mm_sub_ps(CVec_ptr[i], LVec_ptr[i]);
+            *vec_den_0 = _mm_sub_ps(*vec_den_0, RVec_ptr[i]);
+
+
+            //float den_1 = mVec[i] * nVec[i];
+            *vec_den_1 = _mm_mul_ps(mVec_ptr[i] ,nVec_ptr[i]);
+
+            //float den = den_0 / den_1;
+            *vec_den = _mm_div_ps(*vec_den_0 ,*vec_den_1);
+
+            //FVec[i] = num / (den + 0.01);
+            FVec_ptr[i] = _mm_add_ps(*twosVec_ptr, *twosVec_ptr);
+            //FVec_ptr[i] = _mm_div_ps(*vec_num ,FVec_ptr[i]);
+
+        }
         for (int i = 0; i < N; i++) {
-            float num_0 = LVec[i] + RVec[i];
-            float num_1 = mVec[i] * (mVec[i] - 1.0) / 2.0;
-            float num_2 = nVec[i] * (nVec[i] - 1.0) / 2.0;
-            float num = num_0 / (num_1 + num_2);
-            float den_0 = CVec[i] - LVec[i] - RVec[i];
-            float den_1 = mVec[i] * nVec[i];
-            float den = den_0 / den_1;
-            FVec[i] = num / (den + 0.01);
             maxF = FVec[i] > maxF ? FVec[i] : maxF;
         }
+
 
         double time1 = gettime();
         timeTotal += time1 - time0;
